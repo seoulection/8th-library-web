8b9509dfc31d42be621f75ec0db436a386497d6f
junehoe <br1ansun9@gmail.com>
2020-03-23 11:54:15 -0700

    Adds catch all route to redirect unauthenticated users to the homepage

--- src/UnauthenticatedApp.js
+++ src/UnauthenticatedApp.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import Login from './views/Login';
 
 function UnauthenticatedApp() {
   return (
     <div className="container-fluid">
       <Login />
+      <Route render={() => <Redirect to="/" />} />
     </div>
   );
 }
