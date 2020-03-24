808908b2009f0994e1f17ed2bd1fce1d52581289
junehoe <br1ansun9@gmail.com>
2020-03-24 13:26:58 -0700

    Adds user dashboard

--- src/components/NavbarLinks.js
+++ src/components/NavbarLinks.js
@@ -5,6 +5,7 @@ import GoogleLogoutLink from './GoogleLogoutLink';
 function NavbarLinks() {
   return (
     <ul className="nav navbar-nav navbar-right">
+      <li><Link to="/dashboard">Dashboard</Link></li>
       <li><Link to="/">Listings</Link></li>
       <li><Link to="/books/add">Add Book</Link></li>
       <li><GoogleLogoutLink /></li>
